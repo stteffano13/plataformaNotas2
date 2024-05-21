de1671f50c2a1dd54678233a44994efea03856a1
stteffano13 <tefo.aguayo@gmail.com>
2024-05-21 12:35:06 -0500

    Delete plataforma directory

--- plataforma/src/app/models/matricula.ts
+++
@@ -1,9 +0,0 @@
-export class Matricula {
-    constructor(
-        public _id: String,
-        public codigoE: String,
-        public codigoC: String,
-        public periodo:String,
-        public estado: number
-    ) { }
-}
